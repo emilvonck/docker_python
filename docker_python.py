74184598bd8c4e3fb70860d8c8bbba1464786a66
Emil Vonck <skoglund.emil@gmail.com>
2021-08-31 19:58:08 +0200

    print dict with rich

--- docker_python.py
+++ docker_python.py
@@ -1,5 +1,12 @@
+from rich import print
+
 def main():
-    print("Successful run using own docker image")
+    data = [
+        {
+        f"key{i}": f"value{i}"
+        } for i in range(20)
+    ]
+    print(data)
 
 
 if __name__ == "__main__":
